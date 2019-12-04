1abc65b82efb913f48349a726406d5253e077ab0
Marcelo Badet <marcelo.badet@araujo.com.br>
2019-12-04 07:20:29 -0300

    Final do projeto

--- src/app/jogo-da-velha/jogo-da-velha.component.spec.ts
+++ src/app/jogo-da-velha/jogo-da-velha.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { JogoDaVelhaComponent } from './jogo-da-velha.component';
+import { JogoDaVelhaService } from './shared';
 
 describe('JogoDaVelhaComponent', () => {
   let component: JogoDaVelhaComponent;
@@ -8,18 +9,10 @@ describe('JogoDaVelhaComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ JogoDaVelhaComponent ]
+      declarations: [ JogoDaVelhaComponent ],
+      providers: [ JogoDaVelhaService]
     })
     .compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(JogoDaVelhaComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
